fcad7767da99f9e60c7ccd8102068d4bc96fa96a
Alyxia Sother <alyxia@riseup.net>
2025-10-01 11:25:51 +0200

    Add a Dutch translation file

---
+++ src/translations/nl/calendarize.php
@@ -0,0 +1,44 @@
+<?php
+/**
+ * Calendarize plugin for Craft CMS 3.x
+ *
+ * Calendar element types
+ *
+ * @link      https://union.co
+ * @copyright Copyright (c) 2018 Franco Valdes
+ */
+
+/**
+ * @author    Franco Valdes
+ * @package   Calendarize
+ * @since     1.0.0
+ */
+return [
+    'Calendarize plugin loaded' => 'Calendarize plugin loaded',
+    'Sun' => 'Zo',
+    'Mon' => 'Ma',
+    'Tue' => 'Di',
+    'Wed' => 'Wo',
+    'Thu' => 'Do',
+    'Fri' => 'Vr',
+    'Sat' => 'Za',
+    'to' => 'tot',
+    'Never' => 'Nooit',
+    'On Date' => 'Op Datum',
+    'Daily' => 'Dagelijks',
+    'Weekly' => 'Wekelijks',
+    'Biweekly (every 2 weeks)' => 'Tweewekelijks',
+    'Monthly' => 'Maandelijks',
+    'Yearly' => 'Jaarlijks',
+    'All Day' => 'Hele dag',
+    'Repeats' => 'Herhalend',
+    'Every' => 'Herhaalt zich',
+    'Every month on the' => 'Elke maand op',
+    'Ends Repeat' => 'Herhaling beëindigen',
+    'Date Exceptions' => 'Uitgezonderde datums',
+    'Time Exceptions' => 'Uitgezonderde tijden',
+    'Occurrences' => 'Gebeurtenissen',
+    'Last Occurrence' => 'Laatste gebeurtenis',
+    'Next Occurrence' => 'Volgende gebeurtenis',
+    'Date' => 'Datum'
+];
